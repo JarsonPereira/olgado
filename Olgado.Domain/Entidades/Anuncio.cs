31f5fbdc33c4f2cfd6a4774962a3a6a2fa7833dc
jarson2014@hotmail.com <jarson2014@hotmail.com>
2019-06-15 04:57:04 -0300

    Adicionando vinculo de usuario e anuncio.

--- Olgado.Domain/Entidades/Anuncio.cs
+++ Olgado.Domain/Entidades/Anuncio.cs
@@ -1,6 +1,7 @@
 ﻿using Olgado.Domain.Enum;
 using Olgado.Domain.ValueObjects;
 using Olgado.Shared.Entidades;
+using System;
 using System.Collections.Generic;
 
 
@@ -8,16 +9,8 @@ namespace Olgado.Domain.Entidades
 {
     public class Anuncio:Entidade
     {
-        public Anuncio(string descricao, List<string> imagensUrl, Localizacao localizacao, Animal animal, decimal valor, TipoAnuncio tipo)
-        {
-            Descricao = descricao;
-            ImagensUrl = imagensUrl;
-            Localizacao = localizacao;
-            Animal = animal;
-            Valor = valor;
-            Tipo = tipo;
-        }
 
+        public Guid IDUsuario { get; private set; }
         public string Descricao { get; private set; }
         public List<string> ImagensUrl { get; private set; }
         public Localizacao Localizacao  { get; private set; }
