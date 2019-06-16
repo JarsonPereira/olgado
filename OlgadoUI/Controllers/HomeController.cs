a7596c867d2e612ba431d1ad4a46c9ff1b46813a
jarson2014@hotmail.com <jarson2014@hotmail.com>
2019-06-16 09:00:26 -0300

    Finalizado MVP

--- OlgadoUI/Controllers/HomeController.cs
+++ OlgadoUI/Controllers/HomeController.cs
@@ -13,5 +13,9 @@ namespace OlgadoUI.Controllers
             return View();  
         }
 
+        public ActionResult Anunciar()
+        {
+            return View();
+        }
     }
 }
---
+++ OlgadoUI/Models/Anuncio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OlgadoUI.Models
+{
+    public class Anuncio
+    {
+        public Guid IDUsuario { get;  set; }
+        public string Descricao { get;  set; }
+        public HttpPostedFileBase ImagemUrl { get;  set; }
+        public string Estado { get;  set; }
+        public string Cep { get; private set; }
+        public int QuantidadeAnimal { get; set; }
+        public int TipoAnuncio { get;  set; }
+        public bool Impulsionar { get;  set; }
+        public decimal  ValorTotal { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+
+    }
+}
